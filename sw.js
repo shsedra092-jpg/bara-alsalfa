28f52630524d112fe2cfad4a938a38f4dbc051d8
shsedra092-jpg <shsedra092@gmail.com>
2026-02-04 22:45:10 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
-const CACHE_NAME = "bara-alsalfa-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./app.js"];
+const CACHE_NAME = "bara-alsalfa-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./favicon-32.png"
+];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
     ).then(() => self.clients.claim())
   );
 });
